ffa571a65a6512c478638bbd4a40041177cca7a6
Erick Mendoza <erickml506@gmail.com>
2023-02-05 16:29:54 -0500

    First commint

---
+++ script.js
@@ -0,0 +1,6 @@
+var button = document.getElementById('boton-juegos');
+var bloqueImagenes = document.getElementById('bloque-imagenes');
+
+button.addEventListener('click', function() {
+    bloqueImagenes.style.display = 'block';
+});
